41b14bfc45cee588989abed11b7a172228dfb1ee
Adarsh2425 <82866381+Adarsh2425@users.noreply.github.com>
2023-09-02 09:05:54 +0000

    Pending changes exported from your codespace

--- slicer/slicer.py
+++ slicer/slicer.py
@@ -1,151 +1,90 @@
-import os.path
-from argparse import ArgumentParser
-import time
+"""
+This module contains functions for G-code generation.
 
-import librosa
+Author: Your Name
+Date: Date Created
+
+Include additional information about the module's purpose and usage here.
+"""
+
 import numpy as np
-import soundfile
-from scipy.ndimage import maximum_filter1d, uniform_filter1d
+from stl import mesh
 
+# Define the STL file path as a constant (uppercase naming style)
+STL_FILE_PATH = (
+    '/workspaces/IMEX-Independent-Multi-Extrusion-3D-printer-/slicer/'
+    'Stanford_Bunny_sample.stl'
+)
 
-def timeit(func):
-    def run(*args, **kwargs):
-        t = time.time()
-        res = func(*args, **kwargs)
-        print('executing \'%s\' cost %.3fs' % (func.__name__, time.time() - t))
-        return res
-    return run
+# Load the STL file
+MESH_DATA = mesh.Mesh.from_file(STL_FILE_PATH)
 
+# Define slicing parameters
+LAYER_HEIGHT = 0.2  # Layer height in millimeters
 
-# @timeit
-def _window_maximum(arr, win_sz):
-    return maximum_filter1d(arr, size=win_sz)[win_sz // 2: win_sz // 2 + arr.shape[0] - win_sz + 1]
+# Calculate the number of layers
+MAX_Z = np.max(MESH_DATA.vectors[:, :, 2])  # Access z-coordinates of all vertices
+MIN_Z = np.min(MESH_DATA.vectors[:, :, 2])
+NUM_LAYERS = int((MAX_Z - MIN_Z) / LAYER_HEIGHT)
 
+# Define G-code parameters
+EXTRUSION_WIDTH = 0.4  # Extrusion width in millimeters
+PRINT_SPEED = 50  # Print speed in millimeters per second
 
-# @timeit
-def _window_rms(arr, win_sz):
-    filtered = np.sqrt(uniform_filter1d(np.power(arr, 2), win_sz) - np.power(uniform_filter1d(arr, win_sz), 2))
-    return filtered[win_sz // 2: win_sz // 2 + arr.shape[0] - win_sz + 1]
+# Create an empty G-code string
+GCODE = []
 
+# Loop through each layer
+for LAYER in range(NUM_LAYERS):
+    # Calculate the current layer height
+    CURRENT_Z = MIN_Z + LAYER * LAYER_HEIGHT
 
-def level2db(levels, eps=1e-12):
-    return 20 * np.log10(np.clip(levels, a_min=eps, a_max=1))
-
-
-def _apply_slice(audio, begin, end):
-    if len(audio.shape) > 1:
-        return audio[:, begin: end]
-    else:
-        return audio[begin: end]
-
-
-class Slicer:
-    def __init__(self,
-                 sr: int,
-                 db_threshold: float = -40,
-                 min_length: int = 5000,
-                 win_l: int = 300,
-                 win_s: int = 20,
-                 max_silence_kept: int = 500):
-        self.db_threshold = db_threshold
-        self.min_samples = round(sr * min_length / 1000)
-        self.win_ln = round(sr * win_l / 1000)
-        self.win_sn = round(sr * win_s / 1000)
-        self.max_silence = round(sr * max_silence_kept / 1000)
-        if not self.min_samples >= self.win_ln >= self.win_sn:
-            raise ValueError('The following condition must be satisfied: min_length >= win_l >= win_s')
-        if not self.max_silence >= self.win_sn:
-            raise ValueError('The following condition must be satisfied: max_silence_kept >= win_s')
-
-    @timeit
-    def slice(self, audio):
-        if len(audio.shape) > 1:
-            samples = librosa.to_mono(audio)
-        else:
-            samples = audio
-        if samples.shape[0] <= self.min_samples:
-            return [audio]
-        # get absolute amplitudes
-        abs_amp = np.abs(samples - np.mean(samples))
-        # calculate local maximum with large window
-        win_max_db = level2db(_window_maximum(abs_amp, win_sz=self.win_ln))
-        sil_tags = []
-        left = right = 0
-        while right < win_max_db.shape[0]:
-            if win_max_db[right] < self.db_threshold:
-                right += 1
-            elif left == right:
-                left += 1
-                right += 1
-            else:
-                if left == 0:
-                    split_loc_l = left
-                else:
-                    sil_left_n = min(self.max_silence, (right + self.win_ln - left) // 2)
-                    rms_db_left = level2db(_window_rms(samples[left: left + sil_left_n], win_sz=self.win_sn))
-                    split_win_l = left + np.argmin(rms_db_left)
-                    split_loc_l = split_win_l + np.argmin(abs_amp[split_win_l: split_win_l + self.win_sn])
-                if len(sil_tags) != 0 and split_loc_l - sil_tags[-1][1] < self.min_samples and right < win_max_db.shape[0] - 1:
-                    right += 1
-                    left = right
-                    continue
-                if right == win_max_db.shape[0] - 1:
-                    split_loc_r = right + self.win_ln
-                else:
-                    sil_right_n = min(self.max_silence, (right + self.win_ln - left) // 2)
-                    rms_db_right = level2db(_window_rms(samples[right + self.win_ln - sil_right_n: right + self.win_ln], win_sz=self.win_sn))
-                    split_win_r = right + self.win_ln - sil_right_n + np.argmin(rms_db_right)
-                    split_loc_r = split_win_r + np.argmin(abs_amp[split_win_r: split_win_r + self.win_sn])
-                sil_tags.append((split_loc_l, split_loc_r))
-                right += 1
-                left = right
-        if left != right:
-            sil_left_n = min(self.max_silence, (right + self.win_ln - left) // 2)
-            rms_db_left = level2db(_window_rms(samples[left: left + sil_left_n], win_sz=self.win_sn))
-            split_win_l = left + np.argmin(rms_db_left)
-            split_loc_l = split_win_l + np.argmin(abs_amp[split_win_l: split_win_l + self.win_sn])
-            sil_tags.append((split_loc_l, samples.shape[0]))
-        if len(sil_tags) == 0:
-            return [audio]
-        else:
-            chunks = []
-            if sil_tags[0][0] > 0:
-                chunks.append(_apply_slice(audio, 0, sil_tags[0][0]))
-            for i in range(0, len(sil_tags) - 1):
-                chunks.append(_apply_slice(audio, sil_tags[i][1], sil_tags[i + 1][0]))
-            if sil_tags[-1][1] < samples.shape[0] - 1:
-                chunks.append(_apply_slice(audio, sil_tags[-1][1], samples.shape[0]))
-            return chunks
-
-
-def main():
-    parser = ArgumentParser()
-    parser.add_argument('audio', type=str, help='The audio to be sliced')
-    parser.add_argument('--out', type=str, help='Output directory of the sliced audio clips')
-    parser.add_argument('--db_thresh', type=float, required=False, default=-40, help='The dB threshold for silence detection')
-    parser.add_argument('--min_len', type=int, required=False, default=5000, help='The minimum milliseconds required for each sliced audio clip')
-    parser.add_argument('--win_l', type=int, required=False, default=300, help='Size of the large sliding window, presented in milliseconds')
-    parser.add_argument('--win_s', type=int, required=False, default=20, help='Size of the small sliding window, presented in milliseconds')
-    parser.add_argument('--max_sil_kept', type=int, required=False, default=500, help='The maximum silence length kept around the sliced audio, presented in milliseconds')
-    args = parser.parse_args()
-    out = args.out
-    if out is None:
-        out = os.path.dirname(os.path.abspath(args.audio))
-    audio, sr = librosa.load(args.audio, sr=None)
-    slicer = Slicer(
-        sr=sr,
-        db_threshold=args.db_thresh,
-        min_length=args.min_len,
-        win_l=args.win_l,
-        win_s=args.win_s,
-        max_silence_kept=args.max_sil_kept
+    # Create a new layer in the G-code
+    GCODE.append(
+        f"; Layer {LAYER + 1}, Z = {CURRENT_Z:.2f} mm"
     )
-    chunks = slicer.slice(audio)
-    if not os.path.exists(out):
-        os.makedirs(out)
-    for i, chunk in enumerate(chunks):
-        soundfile.write(os.path.join(out, f'%s_%d.wav' % (os.path.basename(args.audio).rsplit('.', maxsplit=1)[0], i)), chunk, sr)
+    GCODE.append(
+        f"G1 Z{CURRENT_Z:.2f} F{PRINT_SPEED}"
+    )
 
+    # Loop through each triangle in the mesh
+    for TRIANGLE in MESH_DATA.vectors:
+        # Get the z-coordinates of the triangle's vertices
+        TRIANGLE_Z = TRIANGLE[:, 2]
 
-if __name__ == '__main__':
-    main()
+        # Calculate the min and max z-coordinates of the triangle
+        MIN_Z_TRIANGLE = min(TRIANGLE_Z)
+        MAX_Z_TRIANGLE = max(TRIANGLE_Z)
+
+        # Check if the triangle intersects with the current layer
+        if MIN_Z_TRIANGLE <= CURRENT_Z <= MAX_Z_TRIANGLE:
+            # Calculate the area of the triangle (cross product method)
+            A, B, C = TRIANGLE[0], TRIANGLE[1], TRIANGLE[2]
+            AB = B - A
+            AC = C - A
+            AREA = 0.5 * np.linalg.norm(np.cross(AB, AC))
+
+            # Calculate the extrusion length based on layer height and triangle area
+            EXTRUSION_LENGTH = LAYER_HEIGHT * AREA
+            # Calculate the extrusion feedrate (speed)
+            EXTRUSION_FEEDRATE = PRINT_SPEED * EXTRUSION_WIDTH / EXTRUSION_LENGTH
+
+            # Create G-code for moving to the triangle's starting point
+            for VERTEX in TRIANGLE:
+                GCODE.append(
+                    f"G1 X{VERTEX[0]:.2f} Y{VERTEX[1]:.2f} Z{CURRENT_Z:.2f} F{PRINT_SPEED}"
+                )
+
+            # Create G-code for extruding along the triangle edges
+            for I in range(3):
+                NEXT_VERTEX = (I + 1) % 3
+                GCODE.append(
+                    f"G1 X{TRIANGLE[I][0]:.2f} Y{TRIANGLE[I][1]:.2f} Z{CURRENT_Z:.2f} "
+                    f"E{EXTRUSION_LENGTH:.2f} F{EXTRUSION_FEEDRATE:.2f}"
+                )
+
+# Save the G-code to a file
+with open('output.gcode', 'w', encoding='utf-8') as GCODE_FILE:
+    GCODE_FILE.write('\n'.join(GCODE))
+
+print('G-code generation complete. Output saved to "output.gcode".')
